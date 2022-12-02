3b13dd28af743f2e39eb8e97fadf97ebc6ad6dfd
i6rahim <i6rahim210@gmail.com>
2022-12-02 23:11:08 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+
+const sr = ScrollReveal({
+    reset: true,
+    origin: 'left',
+    distance: '160px',
+    duration: 2500,
+    delay: 400,
+});
+
+sr.reveal('.header', {origin: 'top'});
+sr.reveal('.home__text');
+sr.reveal('.dofamin');
+sr.reveal('.about__first-text', {origin: 'right'});
+sr.reveal('.about__second-text', {origin: 'left'});
+sr.reveal('.aboutsecond__main', {origin: 'left'});
+sr.reveal('.aboutsecond__submain', {origin: 'right'});
+sr.reveal('.seriously__text', {origin: 'right'});
+sr.reveal('.want__text', {origin: 'left'});
+sr.reveal('.skills__text-first', {origin: 'left'});
+sr.reveal('.skills__text-second', {origin: 'right'});
+sr.reveal('.skills__text-third', {origin: 'left'});
+sr.reveal('.help__text', {origin: 'left'});
+sr.reveal('.be__text', {origin: 'right'});
+sr.reveal('.alone__text', {origin: 'top'});
+sr.reveal('.telegram__text', {origin: 'left'});
